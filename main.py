1f239fecf2713ebc09d5f439aa642cbf611d6144
Micaela1509 <micaela.dc15@gmail.com>
2025-01-10 11:53:46 +0200

    Deploy to PP v2

--- main.py
+++ main.py
@@ -6,10 +6,7 @@ print('debug')
 #Micaela makes changes
 print('Hello from MC')
 
-<<<<<<< HEAD
 # Christiaan makes a change
 print('heyyyyyy')
-=======
 # making a variable
 x = 5
->>>>>>> 2377a29adafb5651f6091f4ef02b2b4b708aee31
